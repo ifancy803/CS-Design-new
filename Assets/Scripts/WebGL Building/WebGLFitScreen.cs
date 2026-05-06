bb791892ef2af6a155c6477d70dbbded9778d029
ifancy803 <2354587894@qq.com>
2026-05-06 21:52:38 +0800

    新增彩色化插件，打包webgl

---
+++ Assets/Scripts/WebGL Building/WebGLFitScreen.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class WebGLFitScreen : MonoBehaviour
+{
+    void Start()
+    {
+        Screen.fullScreen = false;
+    }
+
+    void Update()
+    {
+        // WebGL 不建议在 Update 里频繁 SetResolution
+        // 自适应交给 html/css 处理即可
+    }
+}
